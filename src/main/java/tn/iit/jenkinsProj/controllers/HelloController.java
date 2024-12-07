a1ea9ec8f662d30e71ca14d5275abee48c163944
abdellah <abdellahdhahri0@gmail.com>
2024-12-07 20:31:13 +0100

    third commit

--- src/main/java/tn/iit/jenkinsProj/controllers/HelloController.java
+++ src/main/java/tn/iit/jenkinsProj/controllers/HelloController.java
@@ -19,6 +19,10 @@ public class HelloController {
 	public String sayHello() {
 		return "hello from controller";
 	}
+	@GetMapping("/bonjour")
+	public String sayBonjour() {
+		return "bonjour from controller";
+	}
 	
 
 }
